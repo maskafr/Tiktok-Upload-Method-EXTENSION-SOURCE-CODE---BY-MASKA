f362cf70f282d8f12f6d67d41e298f517bb06f99
maskafr <maskatiktok@outlook.com>
2026-04-07 22:32:23 -0400

    Delete Maska Upload Method directory

--- Maska Upload Method/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.get(['sysActive'], (res) => {
-        if (res.sysActive === undefined) {
-            chrome.storage.local.set({ sysActive: false });
-        }
-    });
-});
